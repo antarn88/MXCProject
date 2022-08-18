6a488a149208e8a96d17af228258c120518903db
Arnold <antaloczi.arnold@gmail.com>
2022-08-18 09:03:01 +0200

    WebApp FE - Fix header test

--- WebApp/frontend/src/components/Header/Header.test.tsx
+++ WebApp/frontend/src/components/Header/Header.test.tsx
@@ -1,12 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import Header from './Header';
 
-describe('Header render test', () => {
-  test('Header render test', async () => {
-    render(
-      <div>
-        <Header />
-      </div>
-    );
+describe('Header render test with router', () => {
+  test('Load header and router for it and click logout button', () => {
+    // Arrange
+    render(<Header />, { wrapper: MemoryRouter });
+
+    // Act
+    fireEvent.click(screen.getByText('Logout'));
+
+    // Assert
+    expect(screen.getByRole('button')).toBeTruthy();
   });
 });
