d3159e97262a3a1cad61b5502915ca1a5c063e9f
Arnold <antaloczi.arnold@gmail.com>
2022-09-09 19:42:41 +0200

    WebAPP BE - Add mongoose and dotenv

--- WebApp/backend/src/app.module.ts
+++ WebApp/backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
