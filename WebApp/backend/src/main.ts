b9bc0a723756cdc871e0f2131a281295a4c9ee6c
Arnold <antaloczi.arnold@gmail.com>
2022-09-16 20:46:05 +0200

    WebApp BE - Add api prefix and base swagger

--- WebApp/backend/src/main.ts
+++ WebApp/backend/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as morgan from 'morgan';
 
 import { AppModule } from './app.module';
@@ -7,9 +8,19 @@ import { GLOBAL_VALIDATION_PIPE } from './app.utils';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(GLOBAL_VALIDATION_PIPE);
   app.use(morgan('tiny'));
+
+  const config = new DocumentBuilder()
+    .setTitle('MXC Project - WebApp API')
+    .setDescription('MXC Project - WebApp API swagger documentation')
+    .addTag('users')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs/swagger', app, document);
 
   await app.listen(5000);
 }
