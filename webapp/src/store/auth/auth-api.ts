0f03eda62872fb987466421f88639312dd69560b
Arnold <antaloczi.arnold@gmail.com>
2022-09-20 20:58:30 +0200

    WebApp - Add types

--- webapp/src/store/auth/auth-api.ts
+++ webapp/src/store/auth/auth-api.ts
@@ -4,7 +4,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ILoginResponse } from '../../interfaces/auth/login-response.interface';
 import { ILoginRequest } from '../../interfaces/auth/login-request.interface';
 
-export const login = createAsyncThunk('auth/login', async (loginData: ILoginRequest) => {
+export const login = createAsyncThunk('auth/login', async (loginData: ILoginRequest): Promise<ILoginResponse> => {
   const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, {
     username: loginData.username,
     password: loginData.password,
@@ -12,6 +12,6 @@ export const login = createAsyncThunk('auth/login', async (loginData: ILoginRequ
   return response.data as ILoginResponse;
 });
 
-export const logout = createAsyncThunk('auth/logout', async () => {
+export const logout = createAsyncThunk('auth/logout', async (): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, 0));
 });
