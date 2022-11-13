fc79a254a12fab2e35a55d1b8584d82b375a1260
Arnold <antaloczi.arnold@gmail.com>
2022-11-13 18:58:08 +0100

    MobileApp - Update header

--- mobileapp/src/components/Header/Header.styles.ts
+++ mobileapp/src/components/Header/Header.styles.ts
@@ -2,8 +2,30 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   headerContainer: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 10,
     backgroundColor: '#F5F5F5',
     fontWeight: 'bold',
     height: 80,
+  },
+  rightSideContainer: {
+    flexDirection: 'row',
+  },
+  labels: {
+    marginRight: 6,
+    justifyContent: 'center',
+  },
+  image: {
+    borderRadius: 30,
+  },
+  emailLabel: {
+    fontWeight: '900',
+    fontSize: 14,
+  },
+  logoutLabel: {
+    textAlign: 'right',
+    fontSize: 14,
   },
 });
--- mobileapp/src/components/Header/Header.tsx
+++ mobileapp/src/components/Header/Header.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {useNavigate} from 'react-router-native';
 
 import {styles} from './Header.styles';
 
 const Header = (): JSX.Element => {
+  const navigate = useNavigate();
+  const email = 'nintendo@lakatos.hu';
+
+  const onPressLogo = useCallback((): void => navigate('/'), [navigate]);
+  const onPressLogout = useCallback((): void => console.log('Logging out...'), []);
+
   return (
     <View style={styles.headerContainer}>
-      <Text>Header</Text>
+      <View>
+        <TouchableOpacity onPress={onPressLogo}>
+          <Image source={require('../../../assets/images/logo.png')} />
+        </TouchableOpacity>
+      </View>
+      <View style={[styles.rightSideContainer]}>
+        <View style={[styles.labels]}>
+          <Text style={[styles.emailLabel]}>{email}</Text>
+          <Text style={[styles.logoutLabel]} onPress={onPressLogout}>
+            Logout
+          </Text>
+        </View>
+        <View>
+          <Image source={require('../../../assets/images/placeholder.jpg')} style={[styles.image]} />
+        </View>
+      </View>
     </View>
   );
 };
